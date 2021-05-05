1af60e497f35d7ca4016735c82c8ba8b93ff238f
mk261 <manish.kumar_cs19@gla.ac.in>
2021-05-05 22:46:36 +0530

    eod commit

---
+++ src/definations/MobilePhone.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Manish Kumar
+ *   Date: 05-05-2021
+ *   Time: 22:33
+ *   File: MobilePhone.java
+ */
+
+package definations;
+
+public class MobilePhone extends Phone {
+    public void printContactNumber() {
+        System.out.println(getContactNumber());
+    }
+}
